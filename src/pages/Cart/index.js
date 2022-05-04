28101fcb2af94f10bda9f1f4ca05d2f4992e2cdb
Giovanny L. Silva <didiosilva395@gmail.com>
2022-05-03 22:11:51 -0400

    Estilização do Carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,70 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
 
-// import { Container } from './styles';
+import { Container,ProductTable,Total } from './styles';
 
 function Cart() {
-  return<div > CART </div>;
+  return(
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th/>
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th/>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img 
+                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-6-next-nature-masculino/26/2IC-4365-026/2IC-4365-026_zoom2.jpg?ts=1631636825&ims=100x"
+                alt='Tenis'
+              />
+            </td>
+            <td>
+              <strong>Tenis top</strong>
+              <span>R$123,12</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1"/>
+                </button>
+                <input type='number' readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1"/>
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1"/>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span> TOTAL </span>
+          <strong>R$1920,28</strong>
+        </Total>
+
+      </footer>
+
+    </Container>
+
+  );
 }
 
 export default Cart;
