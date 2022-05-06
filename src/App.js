c08ca59c91bb1ea0f77bb043ebf2ad5a28da3efd
Giovanny L. Silva <didiosilva395@gmail.com>
2022-05-05 21:23:01 -0400

    Reactotron + Redux

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from "./pages/styles/global";
 import Routes  from './routes';
+import './config/ReactotronConfig';
 import Header from "./components/Header";
 import { Provider } from "react-redux";
+
 import store from "./store";
 
 
