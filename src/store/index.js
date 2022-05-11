42785ce0fd70f406f8546ca8d92540d4c147cdd1
Giovanny L. Silva <didiosilva395@gmail.com>
2022-05-10 21:36:57 -0400

    Reactotron + Saga

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,12 @@ import createSagaMiddleware from "@redux-saga/core";
 
 import rootReducer from "./modules/rootReducer";
 import rootSaga from "./modules/rootSaga";
-
-const  sagaMiddleware = createSagaMiddleware();
+const sagaMonitor = process.env.NODE_ENV ==='development' 
+? console.tron.createSagaMonitor()
+: null;
+const  sagaMiddleware = createSagaMiddleware( {
+  sagaMonitor,
+} );
 
 const enhancer = process.env.NODE_ENV ==='development'
 ? compose( 
